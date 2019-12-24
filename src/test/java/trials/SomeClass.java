7eaabcc318eaced47faf64787db06b54ee191c76
Your Name <alexeytimotin@gmail.com>
2019-12-24 19:29:53 +0200

    First commit

--- src/test/java/trials/SomeClass.java
+++ src/test/java/trials/SomeClass.java
@@ -2,11 +2,36 @@ package trials;
 
 import infra.Browser;
 import infra.Browserfactory;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+
 
 public class SomeClass {
     @Test
-    public void firstTest()  {
-        Browserfactory.getDriver(Browser.CHROME);
+    public void firstTest() throws InterruptedException {
+        final WebDriver driver = Browserfactory.getDriver(Browser.CHROME);
+        final JavascriptExecutor jexec = (JavascriptExecutor) driver;
+        final String initTaltab = driver.getWindowHandle();
+
+        jexec.executeScript("window.open('about:blank', '_blank', 'top:500')");
+        final String newWindow = driver
+                .getWindowHandles()
+                .stream()
+                .filter(wh -> !wh.contentEquals(initTaltab))
+                .findFirst()
+                .get();
+        Thread.sleep(3000);
+
+
+//        driver.getWindowHandles().forEach(wh -> System.out.println(wh));
+        driver.switchTo().window(newWindow);
+        driver.get("http://wikipedia.org");
+
+        Thread.sleep(3000);
+        driver.switchTo().window(initTaltab);
+        driver.quit();
+
+
     }
 }
