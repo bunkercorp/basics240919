f87958efbd2b47421a5a20defe1ca844f0813e81
Yuliya Trybunskykh <ul64ita@gmail.com>
2020-01-10 19:36:31 +0200

    added Selenium Utils

---
+++ src/main/java/infra/SeleniumUtils.java
@@ -0,0 +1,21 @@
+package infra;
+
+import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
+
+public class SeleniumUtils {
+    public static String switchToWindow(String windowTitle, WebDriver driver){
+        String windowHandler = null;
+        Set<String> knownHandlers = driver.getWindowHandles();
+        for(String candidateHandler : knownHandlers) {
+            driver.switchTo().window(candidateHandler);
+            String candidateTitle = driver.getTitle();
+            if (candidateTitle.contentEquals(windowTitle)) {
+                return candidateHandler;
+                //break;
+            }
+        }
+        return windowHandler;
+    }
+}
--- src/test/java/trials/SomeClass.java
+++ src/test/java/trials/SomeClass.java
@@ -2,11 +2,40 @@ package trials;
 
 import infra.Browser;
 import infra.Browserfactory;
+import infra.SeleniumUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class SomeClass {
     @Test
-    public void firstTest()  {
+    public void firstTest() throws InterruptedException {
         Browserfactory.getDriver(Browser.CHROME);
+        final WebDriver driver = Browserfactory.getDriver(Browser.CHROME);
+        final JavascriptExecutor jexec = (JavascriptExecutor) driver;
+        final String initialTab = driver.getWindowHandle();
+
+        jexec.executeScript("window.open('about:blank', '_blank', 'top:500')");
+
+
+
+        final String newWindow = driver
+                .getWindowHandles()
+                .stream()
+                .filter(wh -> !wh.contentEquals(initialTab))
+                .findFirst()
+                .get();
+        Thread.sleep(3000);
+
+        driver.switchTo().window(newWindow);
+        driver.get("http://wikipedia.org");
+//        Thread.sleep(3000);
+       driver.switchTo().window(initialTab);
+       String wikiTabHandler = SeleniumUtils.switchToWindow("Wikipedia", driver);
+       if (wikiTabHandler != null)
+           driver.switchTo().window(newWindow);
+//        driver.close();
+
+
     }
 }
