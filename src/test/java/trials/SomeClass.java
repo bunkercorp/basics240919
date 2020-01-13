be50fc451174bdf5f93dfa347db40c6659b5b49b
AndreyMaydanyuk <andrewww.m@gmail.com>
2020-01-13 23:18:40 +0200

    login, assign to user

--- src/test/java/trials/SomeClass.java
+++ src/test/java/trials/SomeClass.java
@@ -1,12 +1,19 @@
 package trials;
 
+import Pages.LoginPage;
 import com.google.gson.internal.bind.util.ISO8601Utils;
 import infra.Browser;
 import infra.Browserfactory;
 import infra.SeleniumUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SomeClass {
     @Test
@@ -15,29 +22,26 @@ public class SomeClass {
         final JavascriptExecutor jexec = (JavascriptExecutor) driver;
         final String intialTab = driver.getWindowHandle();
 
-        jexec.executeScript("window.open('about:blank')");
+        //login
+        driver.manage().window().maximize();
+        driver.get("https://jira.hillel.it/browse/AQA919-5");
+        LoginPage.login(driver);
+        Thread.sleep(500);
 
-        final String newWindow = driver
-                .getWindowHandles()
-                .stream()
-                .filter(wh -> !wh.contentEquals(intialTab))
-                .findFirst()
-                .get();
+        //assign ticket to current user
+        String assignXpath = "//a[@id='assign-to-me']";
+        WebElement assignBtn = driver.findElement(By.xpath(assignXpath));
 
-        Thread.sleep(3000);
+        assignBtn.click();
+        Thread.sleep(1000);
 
-//        driver.getWindowHandles().forEach(wh > System.out.println(wh));
-        driver.switchTo().window(newWindow);
-        driver.get("http://wikipedia.org");
+        //check assignee pop-up msg
+        String popupAssigneeXpath = "//div[@class = 'aui-message closeable aui-message-success aui-will-close']";
+        WebElement popupAssignee = driver.findElement(By.xpath(popupAssigneeXpath));
 
+        if (popupAssignee.getText().equals("AQA919-5 has been assigned."))
+            System.out.println("Ticket assigned successfully!");
 
-
-        Thread.sleep(3000);
-        driver.switchTo().window(intialTab);
-        String wikitabHandler = SeleniumUtils.switchToWindow("Wikipedia", driver);
-        if (wikitabHandler !=null) {
-            driver.switchTo().window(wikitabHandler);
-        }
         driver.quit();
 
     }
