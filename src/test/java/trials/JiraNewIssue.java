9e8746722e78348e2a9124a5fc94a69c9798020e
archick12 <a.a.piluck@gmail.com>
2020-01-14 19:58:03 +0200

    added JSONObject parser

--- src/test/java/trials/JiraNewIssue.java
+++ src/test/java/trials/JiraNewIssue.java
@@ -10,44 +10,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class JiraNewIssue {
 
+
     @Test
     public void postIssue() throws IOException {
+        //  String payload = "{\"fields\":{\"summary\":\"Show must go on\",\"issuetype\":{\"id\":\"10107\"},\"project\":{\"id\":\"12120\"},\"description\":\"We are the championzz\",\"reporter\":{\"name\":\"samigullin.nikita\"},\"priority\":{\"id\":\"5\"},\"labels\":[\"custom_lbl\"]}}";
 
-//        JSONObject payload = new JSONObject();
-//
-//        JSONObject fields = new JSONObject();
-//        fields.put("project", new JSONObject().put("id", "12120"));
-//        fields.put("summary", "Lorem ipsum elit");
-//        fields.put("description", "Lorem ipsum elit description");
-//        fields.put("reporter", new JSONObject().put("name", System.getProperty("jiraUser")));
-//        fields.put("issuetype", new JSONObject().put("id", "10107"));
-//        fields.put("priority", new JSONObject().put("id", "5"));
-//        fields.put("labels", new JSONArray().put("custom_lbl"));
-//
-//
-//        payload.put("fields", fields);
+        JSONObject payload = new JSONObject();
+        JSONObject fields = new JSONObject();
+        fields.put("project", new JSONObject().put("id", "12120"));
+        fields.put("summary", "Lorem ipsum elit");
+        fields.put("description", "Lorem ipsum elit description");
+        fields.put("reporter", new JSONObject().put("name", System.getProperty("jiraUser")));
+        fields.put("issuetype", new JSONObject().put("id", "10107"));
+        fields.put("priority", new JSONObject().put("id", "5"));
+        fields.put("labels", new JSONArray().put("custom_lbl"));
+        payload.put("fields", fields);
 
-        String payload = "{\"fields\":{\"summary\":\"Lorem ipsum elit\",\"issuetype\":{\"id\":\"10107\"},\"project\":{\"id\":\"12120\"},\"description\":\"Lorem ipsum elit description\",\"reporter\":{\"name\":\"samigullin.nikita\"},\"priority\":{\"id\":\"5\"},\"labels\":[\"custom_lbl\"]}}";
-
-                final HttpsURLConnection httpCon = (HttpsURLConnection) new URL("https://jira.hillel.it/rest/api/2/issue").openConnection();
-
+        final HttpsURLConnection httpCon = (HttpsURLConnection) new URL("https://jira.hillel.it/rest/api/2/issue").openConnection();
         httpCon.setDoOutput(true);
         httpCon.setRequestMethod("POST");
         String creds = String.format("%s:%s", System.getProperty("jiraUser"), System.getProperty("jiraPwd"));
-
         httpCon.setRequestProperty("Authorization", String.format("Basic %s", new Base64().encodeToString(creds.getBytes())));
         httpCon.setRequestProperty("Content-Type", "application/json");
-        //      httpCon.setRequestProperty("Content-Length", "" + payload.length());
-        httpCon.setRequestProperty("Cookie", "JSESSIONID=FF498E0E920A9EA0B19053C38B0CA73C");
+        httpCon.setRequestProperty("User-Agent", "Aqa study");
+        httpCon.setRequestProperty("Content-Length", "" + payload.length());
+        httpCon.setRequestProperty("Cookie", "JSESSIONID=8B2B3A0BF2D7BBC91259E9A25C7A2C0E");
         final OutputStreamWriter out = new OutputStreamWriter(httpCon.getOutputStream());
         out.write(payload.toString());
         out.close();
@@ -63,9 +55,8 @@ public class JiraNewIssue {
             result.add(line);
         }
         System.out.println(httpCon.getResponseCode());
-        System.out.println(String.join("", result));
+      JSONObject response = new JSONObject(String.join("", result));
 
-
+        System.out.println(response.toString(5));
     }
-
 }
